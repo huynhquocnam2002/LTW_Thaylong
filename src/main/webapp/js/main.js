c590d43419c526003ed2fd9a770286505678c467
Nguyen Minh Thuan <20130426@st.hcmuaf.edu.vn>
2022-11-03 17:39:08 +0700

    commit again

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,12 +1,5 @@
-const home_li = document.querySelector('#admin-content .home-li')
-const product_li = document.querySelector('#admin-content .product-li')
-const order_li = document.querySelector('#admin-content .order-li')
-const user_li = document.querySelector('#admin-content .user-li')
-const voucher_li = document.querySelector('#admin-content .voucher-li')
-const turnover_li = document.querySelector('#admin-content .turnover-li')
-const design_li = document.querySelector('#admin-content .design-li')
 const list_icon_up = document.querySelectorAll('#admin-content #leftContainer .icon-up')
-const list_extend_button = document.querySelectorAll('#admin-content #rightContainer .manage-products-table .row-table-end .show-all-type-product')
+const list_extend_button = document.querySelectorAll('#admin-content #rightContainer .row-table-end .show-all-type-product')
 const list_checkbox_all_product = document.querySelectorAll('#admin-content #rightContainer .check-all')
 const list_checkbox_product = document.querySelectorAll('#admin-content #rightContainer .is-check')
 const list_checkbox_main_product = document.querySelectorAll('#admin-content #rightContainer .row-table-main .is-check')
@@ -19,23 +12,29 @@ const list_edit_proceed_button = document.querySelectorAll('#admin-content #righ
 const list_accept_proceed_button = document.querySelectorAll('#admin-content #rightContainer .manage-products .accept-proceed')
 const list_delete_proceed_button = document.querySelectorAll('#admin-content #rightContainer .manage-products .delete-proceed')
 const list_add_type_proceed_button = document.querySelectorAll('#admin-content #rightContainer .manage-products .add-type-proceed')
-const list_quantity_type_of_product = document.querySelectorAll('#admin-content #rightContainer .manage-products .row-table-end .quantity-type')
-const list_quantity_product_in_tab = document.querySelectorAll('#admin-content #rightContainer .manage-products .flex-item .quantity')
+const list_quantity_type_of_product = document.querySelectorAll('#admin-content #rightContainer .row-table-end .quantity-type')
+const list_quantity_product_in_tab = document.querySelectorAll('#admin-content #rightContainer .flex-item span.quantity')
 const quantity_checked_product = document.querySelector('#admin-content #rightContainer .manage-products .manage-products-filter-container .quantity')
-const list_copy_button = document.querySelectorAll('#admin-content #rightContainer .manage-products .copy-icon')
+const quantity_checked_order = document.querySelector('#admin-content #rightContainer .manage-orders .manage-orders-filter-container .quantity')
+const list_copy_button = document.querySelectorAll('#admin-content #rightContainer .copy-icon')
 const tool_selling_button = document.querySelector('#admin-content #rightContainer .manage-products .tool-button.selling-button')
 const tool_stop_selling_button = document.querySelector('#admin-content #rightContainer .manage-products .tool-button.stop-selling-button')
 const tool_delete_button = document.querySelector('#admin-content #rightContainer .manage-products .tool-button.delete-button')
-const list_tool_button = document.querySelectorAll('#admin-content #rightContainer .manage-products .tool-button')
-const search_button = document.querySelector('#admin-content #rightContainer .manage-products .input-filter .id-product-filter-div button .search-icon').parentElement
-const search_input = document.querySelector('#admin-content #rightContainer .manage-products .input-filter .id-product-filter-div .id-product-filter').parentElement
+const list_product_tool_button = document.querySelectorAll('#admin-content #rightContainer .manage-products .tool-button')
+const list_order_tool_button = document.querySelectorAll('#admin-content #rightContainer .manage-orders .tool-button')
+const search_product_button = document.querySelector('#admin-content #rightContainer .manage-products .input-filter .id-product-filter-div button .search-icon').parentElement
+const search_order_button = document.querySelector('#admin-content #rightContainer .manage-orders .input-filter .id-order-filter-div button .search-icon').parentElement
+const search_product_input = document.querySelector('#admin-content #rightContainer .manage-products .input-filter .id-product-filter-div .id-product-filter').parentElement
+const search_order_input = document.querySelector('#admin-content #rightContainer .manage-orders .input-filter .id-order-filter-div .id-order-filter').parentElement
 const price_filter_input = document.querySelector('#admin-content #rightContainer .manage-products .input-filter .price-filter-select')
 const category_filter_input = document.querySelector('#admin-content #rightContainer .manage-products .category-filter-select')
 const sort_filter_input = document.querySelector('#admin-content #rightContainer .manage-products .other-filter-select')
 const reset_filter_button = document.querySelector('#admin-content #rightContainer .manage-products .reset-filter-button')
 const list_left_menu_item = document.querySelectorAll('#admin-content #leftContainer .list-tab .list-tab-li .tab-li-container-name')
 const list_right_tab_item = document.querySelectorAll('#admin-content #rightContainer .right-tab')
-const change_info_home_button=document.querySelector('#admin-content #rightContainer .info-table tr td .change-info')
+const change_info_home_button = document.querySelector('#admin-content #rightContainer .info-table tr td .change-info')
+const list_orders_tab = document.querySelectorAll('#admin-content #rightContainer .manage-orders .manage-orders-flex-tab')
+const list_orders_tab_head = document.querySelectorAll('#admin-content #rightContainer .manage-orders .flex-item')
 
 //show list child in left menu admin
 function showChild(li, num) {
@@ -68,14 +67,14 @@ function extendProduct(button) {
 
 function run() {
     //change info of admin
-    change_info_home_button.onclick=function () {
-        let table=change_info_home_button.parentElement.parentElement.parentElement
-        let list_input=table.querySelectorAll('input')
-        if (change_info_home_button.innerHTML!='Ok'){
+    change_info_home_button.onclick = function () {
+        let table = change_info_home_button.parentElement.parentElement.parentElement
+        let list_input = table.querySelectorAll('input')
+        if (change_info_home_button.innerHTML != 'Ok') {
             for (let i of list_input)
                 i.disabled = false
             change_info_home_button.innerHTML = 'Ok'
-        }else {
+        } else {
             for (let i of list_input)
                 i.disabled = true
             change_info_home_button.innerHTML = 'Sửa đổi'
@@ -83,21 +82,21 @@ function run() {
     }
 
     //change tab by left menu
-    for (let i=0; i<list_left_menu_item.length;i++) {
+    for (let i = 0; i < list_left_menu_item.length; i++) {
         list_left_menu_item[i].onclick = function () {
             if (list_left_menu_item[i].parentElement.querySelectorAll('div').length > 1)
                 showChild(list_left_menu_item[i], 2)
-            for (let j of list_left_menu_item){
-                let parent_li=j.parentElement.parentElement
-                if (parent_li.className.indexOf('tab-active')!=-1)
-                    parent_li.className=parent_li.className.slice(0, parent_li.className.indexOf('tab-active')-1)
+            for (let j of list_left_menu_item) {
+                let parent_li = j.parentElement.parentElement
+                if (parent_li.className.indexOf('tab-active') != -1)
+                    parent_li.className = parent_li.className.slice(0, parent_li.className.indexOf('tab-active') - 1)
             }
-            list_left_menu_item[i].parentElement.parentElement.className+=' tab-active'
-            for (let j of list_right_tab_item){
-                if (j.className.indexOf('active-right-tab')!=-1)
-                    j.className=j.className.slice(0, j.className.indexOf('active-right-tab')-1)
+            list_left_menu_item[i].parentElement.parentElement.className += ' tab-active'
+            for (let j of list_right_tab_item) {
+                if (j.className.indexOf('active-right-tab') != -1)
+                    j.className = j.className.slice(0, j.className.indexOf('active-right-tab') - 1)
             }
-            list_right_tab_item[i].className+=' active-right-tab'
+            list_right_tab_item[i].className += ' active-right-tab'
 
         }
     }
@@ -158,7 +157,7 @@ function run() {
             else
                 for (let j of list)
                     j.checked = false
-            activeToolButton()
+            activeToolButtonProduct()
         }
     }
     for (let i of list_checkbox_child_product) {
@@ -186,7 +185,7 @@ function run() {
                 if (is_checked_all) check_all.checked = true
                 else check_all.checked = false
             }
-            activeToolButton()
+            activeToolButtonProduct()
         }
     }
     for (let i of list_checkbox_main_product) {
@@ -196,6 +195,7 @@ function run() {
             if (i.checked)
                 for (let j of list)
                     j.checked = true
+
             else
                 for (let j of list)
                     j.checked = false
@@ -207,7 +207,7 @@ function run() {
             }
             if (is_check_all) check_all.checked = true
             else check_all.checked = false
-            activeToolButton()
+            activeToolButtonProduct()
         }
     }
 
@@ -220,7 +220,20 @@ function run() {
             active_tab.className = active_tab.className.slice(0, active_tab.className.indexOf('active-product-tab'))
             list_product_tab_head[i].className += ' active-tab'
             list_product_tab[i].className += ' active-product-tab'
-            activeToolButton()
+            activeToolButtonProduct()
+        }
+    }
+
+    //change order tab
+    for (let i = 0; i < list_orders_tab_head.length; i++) {
+        list_orders_tab_head[i].onclick = function () {
+            let active_tab_head = document.querySelector('#admin-content #rightContainer .manage-orders .active-tab')
+            let active_tab = document.querySelector('#admin-content #rightContainer .manage-orders .active-order-tab')
+            active_tab_head.className = active_tab_head.className.slice(0, active_tab_head.className.indexOf('active-tab') - 1)
+            active_tab.className = active_tab.className.slice(0, active_tab.className.indexOf('active-order-tab') - 1)
+            list_orders_tab_head[i].className += ' active-tab'
+            list_orders_tab[i].className += ' active-order-tab'
+            activeToolButtonOrder()
         }
     }
 
@@ -354,17 +367,20 @@ function run() {
     for (let i = 0; i < list_quantity_product_in_tab.length; i++) {
         let num = 0
         if (list_product_tab[i] != undefined) {
-            let list_table = list_product_tab[i].querySelectorAll('.table-row-div-product table')
+            let list_table = list_product_tab[i].querySelectorAll('.manage-products-table')
             for (let j of list_table) {
-                num += j.querySelectorAll('tr').length - 2
+                num += j.querySelectorAll('.row-table-child').length
             }
             list_quantity_product_in_tab[i].innerHTML = num + ''
+        } else {
+            let list_table = list_orders_tab[i-list_product_tab.length].querySelectorAll('.manage-order-table')
+            list_quantity_product_in_tab[i].innerHTML = list_table.length+''
         }
     }
 
     //quantity product is checked
 
-    numOfChecked()
+    numOfCheckedProduct()
     //copy ID
     for (let i of list_copy_button) {
         i.onclick = function () {
@@ -425,16 +441,30 @@ function run() {
     }
 
     //tool: search product
-    search_button.onclick = function () {
-        let parent = search_button.parentElement
+    search_product_button.onclick = function () {
+        let parent = search_product_button.parentElement
         let input_text = parent.querySelector('.id-product-filter').value
         findProductByNameOrId(input_text)
     }
-    search_input.onkeydown = function (event) {
+    search_product_input.onkeydown = function (event) {
         if (event.key == 'Enter') {
-            let parent = search_input.parentElement
+            let parent = search_product_input.parentElement
             let input_text = parent.querySelector('.id-product-filter').value
             findProductByNameOrId(input_text)
+        }
+    }
+
+    //tool: search order by order-id or product-id or user-id
+    search_order_button.onclick = function () {
+        let parent = search_order_button.parentElement
+        let input_text = parent.querySelector('.id-order-filter').value
+        findOrderByNameOrId(input_text)
+    }
+    search_order_input.onkeydown = function (event) {
+        if (event.key == 'Enter') {
+            let parent = search_order_input.parentElement
+            let input_text = parent.querySelector('.id-order-filter').value
+            findOrderByNameOrId(input_text)
         }
     }
 
@@ -685,26 +715,79 @@ function findProductByNameOrId(input_text) {
     }
 }
 
-function activeToolButton() {
-    let num = numOfChecked()
+function findOrderByNameOrId(input_text) {
+    let manage_orders = document.querySelector('.manage-orders')
+    let list_order = manage_orders.querySelectorAll('.active-order-tab .table-row-div-order .manage-order-table')
+    for (let i of list_order) {
+        let isContainProductName=false,isContainProductId=false
+        let order_id=i.querySelector('.order-id .id-span').innerHTML
+        let list_product_name = i.querySelectorAll('.product-name')
+        let list_product_id= i.querySelectorAll('.product-id .id-span')
+        let user_name=i.querySelector('.user-name').innerHTML
+        let user_id=i.querySelector('.user-id .id-span').innerHTML
+        for (let i of list_product_name){
+            if (i.innerHTML.indexOf(input_text)!=-1){
+                isContainProductName=true
+                break
+            }
+        }
+        for (let i of list_product_id){
+            if (i.innerHTML.indexOf(input_text)!=-1){
+                isContainProductId=true
+                break
+            }
+        }
+        if (order_id.indexOf(input_text) == -1 && user_id.indexOf(input_text) == -1 && user_name.indexOf(input_text) == -1 && !isContainProductName && !isContainProductId) {
+            i.style.display = 'none'
+        } else {
+            i.style.display = 'table'
+        }
+    }
+}
+
+function activeToolButtonProduct() {
+    let num = numOfCheckedProduct()
     if (num != 0) {
-        for (let i of list_tool_button) {
+        for (let i of list_product_tool_button) {
             i.disabled = false
         }
     } else {
-        for (let i of list_tool_button) {
+        for (let i of list_product_tool_button) {
             i.disabled = true
         }
     }
 }
 
-function numOfChecked() {
+function activeToolButtonOrder() {
+    let num = numOfCheckedOrder()
+    if (num != 0) {
+        for (let i of list_order_tool_button) {
+            i.disabled = false
+        }
+    } else {
+        for (let i of list_order_tool_button) {
+            i.disabled = true
+        }
+    }
+}
+
+function numOfCheckedProduct() {
     let res = 0
     let active_tab = document.querySelector('#admin-content #rightContainer .manage-products .active-product-tab')
     let list_checkbox = active_tab.querySelectorAll('.row-table-child input[type=checkbox]')
     for (let i of list_checkbox)
         if (i.checked) res++
     quantity_checked_product.innerHTML = res + ''
+    return res
+}
+
+function numOfCheckedOrder() {
+    let res = 0
+    let active_tab = document.querySelector('#admin-content #rightContainer .manage-orders .active-order-tab')
+    let list_checkbox = active_tab.querySelectorAll('.row-table-child input[type=checkbox]')
+    for (let i of list_checkbox)
+        if (i.checked) res++
+    quantity_checked_order.innerHTML = res + ''
     return res
 }
 
