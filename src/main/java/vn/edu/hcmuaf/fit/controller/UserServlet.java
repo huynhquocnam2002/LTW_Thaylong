9df04053f346b518bf42508d4ccc35f3b78af891
Nguyen Minh Thuan <20130426@st.hcmuaf.edu.vn>
2022-11-27 05:05:31 +0700

    load user: info, order, voucher

--- src/main/java/vn/edu/hcmuaf/fit/controller/UserServlet.java
+++ src/main/java/vn/edu/hcmuaf/fit/controller/UserServlet.java
@@ -1,13 +1,17 @@
+package vn.edu.hcmuaf.fit.controller;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import vn.edu.hcmuaf.fit.model.User;
+
 import java.io.IOException;
 
 @WebServlet(name = "UserServlet", value = "/UserServlet")
 public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 
     }
 
