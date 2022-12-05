ea0c88b32fb3d66fdacfe35700c5bf42006bfa6f
namdeptrai <20130074@st.hcmuaf.edu.vn>
2022-12-05 16:55:30 +0700

    add Project w

--- src/main/java/vn/edu/hcmuaf/fit/DAO/ProductDAO.java
+++ src/main/java/vn/edu/hcmuaf/fit/DAO/ProductDAO.java
@@ -68,7 +68,7 @@ public class ProductDAO {
         }
         return res;
     }
-    // lấy ra những sản phẩm mới thêm
+    // lấy ra những sản phẩm mới thêm cua danh muc
     public static Set<Product> getProductsByCatorynew (String name) throws SQLException, ClassNotFoundException {
         DataDB db = new DataDB();
         Set<Product> res = new HashSet<Product>();
@@ -84,6 +84,37 @@ public class ProductDAO {
         return res;
     }
 
+    // lấy ra những sản phẩm mới thêm
+    public static Set<Product> getProductsnew (String id) throws SQLException, ClassNotFoundException {
+        DataDB db = new DataDB();
+        Set<Product> res = new HashSet<Product>();
+        PreparedStatement sta = db.getStatement("SELECT * FROM product WHERE product.ID_CATEGORY = (SELECT product.ID_CATEGORY FROM   product WHERE product.ID = ? ) GROUP BY product.`NAME` LIMIT 5");
+        sta.setString(1, id);
+        ResultSet rs = sta.executeQuery();
+        while (rs.next()) {
+            res.add(new Product(rs.getString(1), rs.getString(2), rs.getLong(3),
+                    rs.getString(4), rs.getInt(5), rs.getString(6), rs.getString(7),
+                    rs.getString(8), rs.getInt(9), rs.getDate(10), rs.getInt(11), rs.getString(12)));
+        }
+
+        return res;
+    }
+
+    // lấy ra những sản phẩm lien quan
+    public static Set<Product> getProductsconnection (String id) throws SQLException, ClassNotFoundException {
+        DataDB db = new DataDB();
+        Set<Product> res = new HashSet<Product>();
+        PreparedStatement sta = db.getStatement("SELECT * FROM product WHERE product.ID_CATEGORY = (SELECT product.ID_CATEGORY FROM   product WHERE product.ID = ? ) GROUP BY product.`NAME`");
+        sta.setString(1, id);
+        ResultSet rs = sta.executeQuery();
+        while (rs.next()) {
+            res.add(new Product(rs.getString(1), rs.getString(2), rs.getLong(3),
+                    rs.getString(4), rs.getInt(5), rs.getString(6), rs.getString(7),
+                    rs.getString(8), rs.getInt(9), rs.getDate(10), rs.getInt(11), rs.getString(12)));
+        }
+
+        return res;
+    }
     // lấy sản phẩm theo danh mục ORDER BY DESC
     public static Set<Product> getProductsByCatoryDESC(String name) throws SQLException, ClassNotFoundException {
         DataDB db = new DataDB();
